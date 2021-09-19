96c4de343a59ebe4a30dda88d475f8604099a8d9
chiatzu <alan421810@gmail.com>
2021-09-19 16:10:05 +0800

    datapush
    datapush

---
+++ map.js
@@ -0,0 +1,32 @@
+//會放三個結構 data/methods/mounted
+Vue.createApp({
+    data() {
+        return {
+            courseFilter: 'all',
+            q: '',
+            c_dbt: 'false',
+            CourseList: C_List,
+
+
+        }
+    }, //data
+    methods: {
+
+    }, //methods
+    computed: {
+        filterCourseList: function filterList() {
+            query = this.q.trim()
+            if (query != '') { return this.CourseList.filter((item) => 
+                item.course.indexOf(query)  != -1 || 
+                item.teacher.indexOf(query) != -1 || 
+                item.des.indexOf(query) !=-1
+                )}
+            if (this.c_dbt == true) { return this.CourseList.filter((item) => item.Dtag == "DBT") }
+            else { return this.CourseList }
+        }// filterList()
+    }, //computed
+    mounted: function() {
+        console.log(this.text)
+        // 適合初始化資料
+    },
+}).mount('#app')
